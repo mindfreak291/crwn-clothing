623f3c3c937c4b06650f5168f7486e3e0c9f531f
sahil <sahil.walecha24@gmail.com>
2020-10-05 04:57:52 +0530

    adding project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import HomePage from './Component/HomePage/homepage.component'
+function App() {
+  return (
+    <div className="App">
+    <HomePage/>
+    </div>
+  );
+}
+
+export default App;
