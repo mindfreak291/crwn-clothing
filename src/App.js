dc0e3e850853dfc6235f6310d2ee10eab0b4fd8f
sahil <sahil.walecha24@gmail.com>
2020-10-05 06:18:40 +0530

    home page broken into components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './Component/HomePage/homepage.component'
+import HomePage from './PageComponent/homepage.component'
 function App() {
   return (
     <div className="App">
---
+++ src/PageComponent/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../PageComponent/homepage.scss'
+import Directory from '../Component/directory/directory.component'
+const HomePage = ()=>(
+<div className='homepage'>
+<Directory/>
+</div>
+);
+
+export default HomePage;
